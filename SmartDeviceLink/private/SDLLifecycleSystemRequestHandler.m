174e5c71071de2256aa7499e4f97c44ea191e80e
Shinichi Watanabe <swatanabe@xevo.com>
2020-10-13 14:03:56 +0900

    Add debug noise to LifecycleSystemRequestHandler.

--- SmartDeviceLink/private/SDLLifecycleSystemRequestHandler.m
+++ SmartDeviceLink/private/SDLLifecycleSystemRequestHandler.m
@@ -77,6 +77,7 @@ typedef void (^URLSessionDownloadTaskCompletionHandler)(NSURL *location, NSURLRe
 #pragma mark - Handle OnSystemRequest
 - (void)sdl_handleSystemRequestLaunchApp:(SDLOnSystemRequest *)request {
     NSURL *urlScheme = [NSURL URLWithString:request.url];
+    NSLog(@"sdl_handleSystemRequestLaunchApp: url=%@", request.url);
     if (urlScheme == nil) {
         SDLLogW(@"System request LaunchApp failed: invalid URL sent from module: %@", request.url);
         return;
